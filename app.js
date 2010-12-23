1205e1e0b71d4434a8dc4cf452cbd699ad2e8ed1
sugyan <sugi1982@gmail.com>
2010-12-24 00:45:34 +0900

    use ejs

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 var app = require('express').createServer();
+var ejs = require('ejs');
+
+app.set('view engine', 'ejs');
+app.set('view options', { layout: false });
 
 app.get('/', function(req, res) {
     console.log('/');
-    res.send('hello world');
+    res.render('index', '');
 });
 
 app.listen(3000);
