6224bc76dd6cc283b678a3084810197dd62aadd1
sugyan <sugi1982@gmail.com>
2010-12-27 09:36:22 +0900

    prepend messages

--- static/js/client.js
+++ static/js/client.js
@@ -1,16 +1,21 @@
-var socket = new io.Socket(null, { port: port });
-socket.connect();
-socket.on('connect', function() {
-    console.log('connect');
-}) 
-socket.on('message', function(msg) {
-    console.log('message: ' + msg);
-}) 
-socket.on('disconnect', function(){
-    console.log('disconnect');
-}) 
+$(function() {
+    var socket = new io.Socket(null, { port: port });
+    socket.connect();
+    socket.on('connect', function() {
+        console.log('connect');
+    });
+    socket.on('message', function(msg) {
+        var date = new Date();
+        $('#list').prepend($('<dt>' + date + '</dt><dd>' + msg + '</dd>'));
+    });
+    socket.on('disconnect', function(){
+        console.log('disconnect');
+    });
 
-function onSubmit() {
-    socket.send(document.getElementById('message').value);
-    return false;
-}
+    $('#form').submit(function() {
+        var message = $('#message');
+        socket.send(message.val());
+        message.attr('value', '');
+        return false;
+    });
+});
